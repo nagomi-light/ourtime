5178b1191d8e0878bd019c2174b09d3a5435bc46
nagomi-light <nagomilight@gmail.com>
2026-01-04 19:06:07 +0900

    ログインテストの実装

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_secure_password
     
-    has_many :team_members
-    has_many :teams, through: :team_members
-    has_many :owned_teams, class_name: 'Team', foreign_key: 'owner_id'
+  has_many :team_members
+  has_many :teams, through: :team_members
+  has_many :owned_teams, class_name: 'Team', foreign_key: 'owner_id'
 end
