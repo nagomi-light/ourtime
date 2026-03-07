73c88d2c892b80edba15c095ae74e030527d64a5
nagomi-light <nagomilight@gmail.com>
2026-03-08 00:08:33 +0900

    チームカレンダー用にテスト追加

--- src/spec/rails_helper.rb
+++ src/spec/rails_helper.rb
@@ -74,4 +74,23 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Deviseのテスト用
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
+
+Capybara.server = :puma, { Silent: true }
+Capybara.server_host = "0.0.0.0"
+Capybara.server_port = 3001
+Capybara.app_host = "http://127.0.0.1:3001"
