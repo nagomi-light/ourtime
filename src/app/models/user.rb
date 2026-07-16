1ed2a8cfa4b1a0e2f7ea28575da165cebaa0d6b6
nagomi-light <nagomilight@gmail.com>
2026-07-17 00:44:51 +0900

    (/models/user_spec.rb)の実装

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   
-  has_many :events
+  has_many :events, dependent: :destroy
   has_many :team_members, dependent: :destroy
   has_many :teams, through: :team_members
   has_many :owned_teams, class_name: 'Team', foreign_key: 'owner_id'
