2c432a677475c8963698f6ca712e29ad3d3690e0
nagomi-light <nagomilight@gmail.com>
2026-07-17 16:05:55 +0900

    (/models/team_spec.rb)の実装

--- src/app/models/team.rb
+++ src/app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :events, dependent: :destroy
   has_many :team_members, dependent: :destroy
   has_many :users, through: :team_members
 end
